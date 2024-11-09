1db2019003f7cec6a9161a5e2e0431a54e2134fc
YHM <hmyang@soft-berry.com>
2024-11-09 13:01:53 +0900

    first commit

---
+++ src/main/java/com/prestudy/hanghae/HanghaeApplication.java
@@ -0,0 +1,13 @@
+package com.prestudy.hanghae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaeApplication.class, args);
+	}
+
+}
