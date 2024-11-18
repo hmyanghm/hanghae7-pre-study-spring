f64720e849b53b146eb8b2e24155057e23072c21
YHM <hmyang@soft-berry.com>
2024-11-18 23:07:26 +0900

    ApiResponse, ExceptionHandler 생성

--- src/main/java/com/prestudy/hanghae/board/BoardController.java
+++ src/main/java/com/prestudy/hanghae/board/BoardController.java
@@ -1,4 +1,30 @@
 package com.prestudy.hanghae.board;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@OpenAPIDefinition(
+        info = @Info(title = "Board API 명세서", version = "v1")
+)
 public class BoardController {
+
+    @PostMapping(value = "/api/v1/board/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "게시판 생성")
+    public ResponseEntity<?> register() {
+
+        return ResponseEntity.ok().build();
+    }
+
 }
